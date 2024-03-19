412bd35b3971dc7be869667e8940b555ad55741f
weichen <160557064+xaiowei11@users.noreply.github.com>
2024-03-19 10:40:27 +0800

    Add files via upload

---
+++ robot.java
@@ -0,0 +1,31 @@
+/*package vv;
+
+import java.util.Scanner;
+
+public class robot 
+{
+	public static void main(String[] args) 
+	{
+		String temp="";
+		Scanner sc = new Scanner (System.in);
+		System.out.println("I am bot, you can talk with me");
+        while(true)
+        {
+        	System.out.println("you talk : ");
+        	temp = sc.nextLine();
+        	
+        	System.out.println("I talk : ");
+        	String resultString = temp.replace("?","").replace("//","!").replace("you","I");
+
+        	System.out.println(resultString);
+
+        	
+        	if (temp.equals("0"))
+        	{
+        		System.out.println("exit system");
+        		break;
+        	}
+        }
+	}
+
+}*/
